5fea7122de2397a09ab47254dc5a4be87483d5bd
Derya Dok <dokderya@gmail.com>
2021-04-10 14:10:58 +0300

    Encapsulation tanımlama

---
+++ 15_OOP_Kapsulleme(Encapsulation)/Elma.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _15_OOP_Kapsulleme_Encapsulation_
+{
+    class Elma
+    {
+        decimal _agirlik;
+        decimal _hacim;
+
+        public void SetAgirlik(decimal agirlik)
+        {
+            if (agirlik > 100 && agirlik < 500)
+            {
+                _agirlik = agirlik;
+            }
+            else
+            {
+                Console.WriteLine("Ağırlık değeri 100 - 500 aralığında olmalıdır");
+            }
+        }
+
+        public decimal GetAgirlik()
+        {
+            return _agirlik;
+        }
+
+        public void SetHacim(decimal hacim)
+        {
+            if (hacim > 5 && hacim < 20)
+            {
+                _hacim = hacim;
+            }
+            else
+            {
+                Console.WriteLine("Hacim değeri 5 - 20 aralığında olmalıdır");
+            }
+        }
+
+        public void GetHacim(string sifre)
+        {
+            if (sifre == "1234")
+            {
+                Console.WriteLine("Hacim: " + _hacim.ToString());
+            }
+
+            Console.WriteLine("Bu veriyi görme yetkiniz yok");
+        }
+
+    }
+}
---
+++ 15_OOP_Kapsulleme(Encapsulation)/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("15_OOP_Kapsulleme(Encapsulation)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("15_OOP_Kapsulleme(Encapsulation)")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1880a4e6-4ee8-4724-bfd9-aae68f685402")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
